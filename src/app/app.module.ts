7582fa965cc7e8bd07835bf2fe44d062b2e81934
mwessen <mrfwn@cin.ufpe.br>
2019-06-21 23:20:55 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,13 +17,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp({
-    apiKey: '####',
-    authDomain: '######',
-    databaseURL: '#####',
-    projectId: '######',
-    storageBucket: '######',
-    messagingSenderId: '#####',
-    appId: '########'
+    apiKey: 'AIzaSyBCOh9pPWdTBFC9uchsPFC_VoIAC9Bdvi4',
+    authDomain: 'attendancelist-f9109.firebaseapp.com',
+    databaseURL: 'https://attendancelist-f9109.firebaseio.com',
+    projectId: 'attendancelist-f9109',
+    storageBucket: 'attendancelist-f9109.appspot.com',
+    messagingSenderId: '423007922491',
+    appId: '1:423007922491:web:3e70fbc4c780d8d2'
   }), AngularFireDatabaseModule , FormsModule , ReactiveFormsModule
 ],
   providers: [
